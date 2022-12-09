badc8e42e3a5a1a822d69ceb0decc9e8abbeb816
Takyu Chan <takyu.chan958@gmail.com>
2022-12-08 22:33:07 -0800

    did project wrong, redoing

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,53 @@
+let cardContainer;
+
+var tasks = [
+    {
+        "title": "home",
+        "color": "blue",
+    },
+    {
+        "title": "city",
+        "color": "green",
+    }];
+
+let createTaskCard = (task) => {
+
+  let card = document.createElement('div');
+  card.className = 'card shadow cursor-pointer';
+
+  let cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  let title = document.createElement('h5');
+  title.innerText = task.title;
+  title.className = 'card-title';
+
+  let color = document.createElement('div');
+  color.innerText = task.color;
+  color.className = 'card-color';
+
+
+  cardBody.appendChild(title);
+  cardBody.appendChild(color);
+  card.appendChild(cardBody);
+  cardContainer.appendChild(card);
+
+}
+let initListOfTasks = () => {
+  if (cardContainer) {
+    document.getElementById('card-container').replaceWith(cardContainer);
+    return;
+  }
+
+  cardContainer = document.getElementById('card-container');
+  tasks.forEach((task) => {
+    createTaskCard(task);
+  });
+};
+
+initListOfTasks(); // Here you go
+<div id='card-container'></div>
+
 const searchInputDropdown = document.getElementById('search-input-dropdown');
 const dropdownOptions = document.querySelectorAll('.input-group-dropdown-item');
 
